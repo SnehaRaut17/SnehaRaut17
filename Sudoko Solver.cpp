e5f35eb4cc2156304798b8285fc46cd8cf9e706a
SnehaRaut17 <rautsr23@gmail.com>
2025-07-20 22:31:49 +0530

    Update Sudoko Solver.cpp

--- Sudoko Solver.cpp
+++ Sudoko Solver.cpp
@@ -1,73 +1,94 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-#define N 9
+const int N = 9;
+int board[N][N];
 
-// Function to check if placing num at board[row][col] is valid
-bool isValid(int board[N][N], int row, int col, int num) {
-    // Check row and column
-    for (int x = 0; x < N; x++) {
-        if (board[row][x] == num || board[x][col] == num)
+// Check if number is valid in row, col, and box
+bool isValid(int row, int col, int num) {
+    for (int i = 0; i < N; i++) {
+        if (board[row][i] == num || board[i][col] == num)
             return false;
     }
 
-    // Check 3x3 box
     int startRow = row - row % 3;
     int startCol = col - col % 3;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i + startRow][j + startCol] == num)
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[startRow + i][startCol + j] == num)
                 return false;
-        }
-    }
 
     return true;
 }
 
-// Backtracking function
-bool solveSudoku(int board[N][N]) {
+// Backtracking function to solve sudoku
+bool solveSudoku() {
     for (int row = 0; row < N; row++) {
         for (int col = 0; col < N; col++) {
             if (board[row][col] == 0) {
                 for (int num = 1; num <= 9; num++) {
-                    if (isValid(board, row, col, num)) {
+                    if (isValid(row, col, num)) {
                         board[row][col] = num;
-                        if (solveSudoku(board))
+                        if (solveSudoku())
                             return true;
-                        board[row][col] = 0; // backtrack
+                        board[row][col] = 0;
                     }
                 }
-                return false; // if no number can be placed
+                return false; // No number fits
             }
         }
     }
-    return true; // all cells filled correctly
+    return true; // Solved
 }
 
-// Function to print board
-void printBoard(int board[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+// Function to read sudoku board from file
+void readFromFile(const string& filename) {
+    ifstream file(filename);
+    if (!file) {
+        cout << "Error opening file!\n";
+        exit(1);
+    }
+
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            file >> board[i][j];
+
+    file.close();
+}
+
+// Function to write sudoku board to file
+void writeToFile(const string& filename) {
+    ofstream file(filename);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j)
+            file << board[i][j] << " ";
+        file << "\n";
+    }
+    file.close();
+}
+
+void printBoard() {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j)
             cout << board[i][j] << " ";
-        }
         cout << endl;
     }
 }
 
 int main() {
-    int board[N][N];
+    readFromFile("sudoku_input.txt");
 
-    cout << "Enter the Sudoku board (use 0 for empty cells):\n";
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++)
-            cin >> board[i][j];
+    cout << "Original Sudoku:\n";
+    printBoard();
 
-    if (solveSudoku(board)) {
+    if (solveSudoku()) {
         cout << "\nSolved Sudoku:\n";
-        printBoard(board);
+        printBoard();
+        writeToFile("sudoku_output.txt");
     } else {
-        cout << "\nNo solution exists or invalid input.\n";
+        cout << "Sudoku cannot be solved.\n";
     }
 
     return 0;
